4f0818345b35da44efa43c9583995960f7f5540a
Damien Redd <damiens.redd@gmail.com>
2023-03-23 11:20:43 -0500

    fixed final

--- py-proj-1/starter/part-4/part_4.py
+++ py-proj-1/starter/part-4/part_4.py
@@ -26,22 +26,22 @@
 ## Now convert the proper data-types front strings to either floats or ints depending on what it is. Feel free to comment out your old function so you don't get an error, or copy/paste and give it a new name.
 
 # Code here
-# def create_book():
-#     book_title = input("What's your favorite book? - ")
-#     book_author = input("Who wrote your favorite book? - ")
-#     book_year = int(input("What year was your favorite book published? - "))
-#     book_rating = float(input("What's the rating of your favorite book - "))
-#     book_pages = int(input("How many pages does your favporite book have? - "))
+def create_book():
+    book_title = input("What's your favorite book? - ")
+    book_author = input("Who wrote your favorite book? - ")
+    book_year = int(input("What year was your favorite book published? - "))
+    book_rating = float(input("What's the rating of your favorite book - "))
+    book_pages = int(input("How many pages does your favporite book have? - "))
     
-#     book_dictionary = {
-#         "title": book_title,
-#         "author": book_author,
-#         "year": book_year,
-#         "rating": book_rating,
-#         "pages": book_pages
-#     }
+    book_dictionary = {
+        "title": book_title,
+        "author": book_author,
+        "year": book_year,
+        "rating": book_rating,
+        "pages": book_pages
+    }
     
-#     return book_dictionary
+    return book_dictionary
 
 # print(create_book())
 
@@ -83,17 +83,16 @@
 ## Now add a while loop to your main menu to keep it alive, and continually asking for input until the user chooses to exit the program. Call the main menu to ensure it functions properly.
 
 # Code here
-fav_books = ["Wizard and Glass", "Ender's Game", "Dune", "The Perfect Shadow"]
+fav_books = [""]
 
 def main_menu():
     active = True
     while active:
-        answer = input("Press 1 to Enter a New Book, Press 2 to See All Books, Press 3 to exit menu. ")
+        answer = input("Press 1 to Enter a New Book, Press 2 to See All Books, Press any other key to exit menu. ")
         if answer == "1":
-            new_book = input("What is the book you want to add? ")
+            new_book = create_book()
             fav_books.append(new_book)
         elif answer == "2":
-            # see_all = input("Would you like to see all available books? ")
             print(fav_books)                
         else:   
             active = False
