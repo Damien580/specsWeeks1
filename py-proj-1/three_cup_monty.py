27aa0e1b606afea5b041a600173060f91e4f9bb5
Damien Redd <damiens.redd@gmail.com>
2023-03-23 08:03:12 -0500

    final working game

--- py-proj-1/three_cup_monty.py
+++ py-proj-1/three_cup_monty.py
@@ -1,27 +1,32 @@
-def player_guess():
+# example = [1,2,3,4,5,6,7]
+from random import shuffle
+# shuffle(example)
+# print(example)
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
+
+# mylist = [" ", "O", " "]
+
+def player_guess ():
     guess = ''
     while guess not in ["0", "1", "2"]:
-        guess = input(" Pick a number: 0, 1 or 2! ")
+        guess = input("Pick a number: 0, 1, or 2 - ")
     return int(guess)
+    
 
-my_index = player_guess()
-print(my_index)
+def check_guess(mylist, guess):
+    if mylist[guess] == 'O':
+            print("Winner, Winner, Chicken Dinner!!!")
+            print(mylist)
+    else:
+            print("Too Bad, Sucker!!!")
+            print(mylist)
 
+print("==================================")
 
-example = [1, 2, 3, 4, 5, 6, 7]
-from random import shuffle
-shuffle(example)
-
-
-def shuffle_cup(my_list):
-    shuffle(my_list)
-    return my_list
-
-result = shuffle_cup(example)
-
-
-my_cups = [" ", "O", " "]
-
-print(shuffle_cup(my_cups))
-
-print('==========================================')
+mylist = [" ", "O", " "]
+mixedup_list = shuffle_list(mylist)
+guess = player_guess()
+check_guess(mixedup_list, guess)
